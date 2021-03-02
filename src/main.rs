690e255e69dfdee141c70f1929bcee8a8e5272af
John Tabash <jrtabash@yahoo.com>
2021-03-01 18:33:38 -0600

    Added command line arguments; --stocks-file and --show-groupby.

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,32 @@ mod portfolio;
 mod sputil;
 mod yfinance;
 
+extern crate clap;
+
+use clap::{Arg, App};
 use portfolio::{reports, stocks_reader, stocks_update};
 
 fn main() {
-    // TODO:JT - Add command line arguments:
-    //   1. stocks file
-    //   2. show groupby
-    let stocks_file = String::from("/tmp/stocks_sample.csv");
-    let show_groupby = true;
+    let parsed_args = App::new("Stock Portfolio Tool")
+        .version("0.0.1")
+        .author("jrtabash <jrtabash@yahoo.com>")
+        .about("Get latest close prices and report performance of stocks in portfolio.")
+        .arg(Arg::with_name("stocks_file")
+             .short("s")
+             .long("stocks")
+             .help("CSV file containing stocks in portfolio, formatted as 'symbol,date,quantity,base_price' including a header line")
+             .required(true)
+             .takes_value(true))
+        .arg(Arg::with_name("show_groupby")
+             .short("g")
+             .long("show-groupby")
+             .help("Show quantities and latest prices grouped by ticker"))
+        .get_matches();
 
-    let reader = stocks_reader::StocksReader::new(stocks_file);
+    let stocks_file = parsed_args.value_of("stocks_file").unwrap();
+    let show_groupby = parsed_args.is_present("show_groupby");
+
+    let reader = stocks_reader::StocksReader::new(String::from(stocks_file));
     match reader.read() {
         Ok(mut stocks) => {
             let count_updated = stocks_update::update_stocks(&mut stocks);
