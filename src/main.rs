d67ab6b6e7b7eb2adb6639249a402b5cf4914b5d
John Tabash <jrtabash@yahoo.com>
2021-05-03 17:25:09 -0500

    Updated main function to use process::exit.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
+use std::process;
 use stock_portfolio::application::Application;
 
 fn main() {
     let mut app = Application::new();
-    match app.run() {
-        true => 0,
-        false => 1
-    };
+    if !app.run() {
+        process::exit(1);
+    }
 }
