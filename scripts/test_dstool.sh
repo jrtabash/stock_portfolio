c58593992c21157d218f64b2521a7ddcedd07d18
John Tabash <jrtabash@yahoo.com>
2021-11-22 19:48:42 -0600

    Add test_dstool shell script

---
+++ scripts/test_dstool.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/bash
+
+# --------------------------------------------------------------------------------
+# Variables
+
+DS_TOOL=sp_dstool
+DS_ROOT=/tmp/
+DS_NAME=test_dstool
+DS_STOCKS=/tmp/test_dstool_stocks.csv
+
+VERBOSE=0
+if [ "$1" == "-v" ]; then
+    VERBOSE=1
+fi
+
+# --------------------------------------------------------------------------------
+# Functions
+
+function run_dstool() {
+    command="${DS_TOOL} -r ${DS_ROOT} -n ${DS_NAME} $@"
+    if [ ${VERBOSE} -eq 1 ]; then
+        echo "${command}"
+    fi
+    ${command}
+    if [ $? -ne 0 ]; then
+        echo "Error: Failed to run command ${command}"
+        exit 1
+    fi
+}
+
+function initialize() {
+    rm -rf ${DS_ROOT}/${DS_NAME}/
+    rm -f ${DS_STOCKS}
+
+    dt=$(date --date "last week" +"%Y-%m-%d")
+    echo "symbol,type,date,quantity,base_price" > ${DS_STOCKS}
+    echo "AAPL,stock,${dt},100,115.50" >> ${DS_STOCKS}
+    echo "DELL,stock,${dt},100,50.25" >> ${DS_STOCKS}
+}
+
+function cleanup() {
+    rm -rf ${DS_ROOT}/${DS_NAME}/
+    rm -f ${DS_STOCKS}
+}
+
+# --------------------------------------------------------------------------------
+# Main
+
+initialize
+
+run_dstool -o create
+run_dstool -o update -s ${DS_STOCKS}
+run_dstool -o check
+run_dstool -o check -y AAPL
+run_dstool -o stat
+run_dstool -o stat -y AAPL
+run_dstool -o drop -y DELL
+run_dstool -o delete
+
+cleanup
