0689e43c0c9c3be44ebc1d7be7347afe4b4e5d48
John Tabash <jrtabash@yahoo.com>
2022-05-09 21:28:03 -0500

    Add index stock type

--- src/sp_lib/portfolio/stock_type.rs
+++ src/sp_lib/portfolio/stock_type.rs
@@ -5,13 +5,15 @@ use std::fmt;
 #[derive(PartialOrd, Ord, PartialEq, Eq)]
 pub enum StockType {
     Stock,
-    ETF
+    ETF,
+    Index
 }
 
 pub fn stocktype2str(st: StockType) -> &'static str {
     match st {
         StockType::Stock => "stock",
-        StockType::ETF => "etf"
+        StockType::ETF => "etf",
+        StockType::Index => "index"
     }
 }
 
@@ -19,6 +21,7 @@ pub fn str2stocktype(ststr: &str) -> Result<StockType, Box<dyn Error>> {
     match ststr.to_lowercase().as_str() {
         "stock" => Ok(StockType::Stock),
         "etf" => Ok(StockType::ETF),
+        "index" => Ok(StockType::Index),
         _ => Err(format!("Unknown stock type '{}'", ststr).into())
     }
 }
@@ -40,13 +43,17 @@ mod tests {
     fn test_stock_type() {
         let stock = StockType::Stock;
         let etf = StockType::ETF;
+        let index = StockType::Index;
         let stock_str = "stock";
         let etf_str = "etf";
+        let index_str = "index";
 
         assert_eq!(stocktype2str(stock), stock_str);
         assert_eq!(stocktype2str(etf), etf_str);
+        assert_eq!(stocktype2str(index), index_str);
         assert!(str2stocktype(&stock_str).unwrap() == stock);
         assert!(str2stocktype(&etf_str).unwrap() == etf);
+        assert!(str2stocktype(&index_str).unwrap() == index);
 
         match str2stocktype("foobar") {
             Ok(_) => assert!(false),
