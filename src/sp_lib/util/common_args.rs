115cb73473f9e4bbf306bf98607a4f10e0cdadd4
John Tabash <jrtabash@yahoo.com>
2023-01-23 17:34:37 -0600

    Update version number 0.9.1

--- src/sp_lib/util/common_args.rs
+++ src/sp_lib/util/common_args.rs
@@ -9,7 +9,7 @@ use crate::util::datetime;
 // Common Version
 
 pub fn app_version() -> &'static str {
-    "0.9.0"
+    "0.9.1"
 }
 
 // --------------------------------------------------------------------------------
